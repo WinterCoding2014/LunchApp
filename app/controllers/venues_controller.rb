d2d6e058499bdf2667156b8c1be52b173f19cbad
Jojo Li <joli@thoughtworks.com>
2014-07-01 16:35:57 +0800

    Create venueController and JSON blob

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,10 @@
 class VenuesController < ApplicationController
 def index
 	@venues=Venue.all
-	render :json =>@venues
+	#render :json =>@venues
+	respond_to do |format|
+		format.html {}
+		format.json { render :json =>@venues}
+	end
 end
 end
