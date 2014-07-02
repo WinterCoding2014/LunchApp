0e2dad308461a2132addb026375bd599079a26ab
Pyrby <pyrbyg@gmail.com>
2014-07-02 12:42:16 +0800

    Table now displays venues from database

--- app/assets/javascripts/modelView.js
+++ app/assets/javascripts/modelView.js
@@ -1,37 +1,43 @@
-var onReady = function() {
-	//viewModel.js
+var onReady = function () {
+    //viewModel.js
 
-    function venue(data) {
-    this.venueName = ko.observable(data.venueName);
-    this.venueDesptn = ko.observable(data.venueDesptn);
-    this.venueAddress = ko.observable(data.venueAddress);
+    //This should load the database rows into the table on the venueList page automatically
+    function AppViewModel() {
+        var self = this;
+        self.venueArray = ko.observableArray();
+        self.loadVenues = function () {
+            $.ajax(
+                {
+                    type: 'GET',
+                    url: '/venues.json',
+                    dataType: "json",
+                    success: function (venueData) {
+                        self.venueArray(venueData);
+                    }
+                });
+        };
+
+        self.newVenue = ko.observable();
+        self.newDesptn = ko.observable();
+        self.newAddress = ko.observable();
+
+        self.addVenue = function () {
+            /*self.venues.push(new venue({ venueName: this.newVenue(), venueDesptn: this.newDesptn(), venueAddress: this.newAddress()}));*/
+            //Add AJAX POST here
+            self.newVenue("");
+            self.newDesptn("");
+            self.newAddress("");
+        };
+
+        self.loadVenues();
+
+       /* function venue(venueData) {
+            this.venueName = ko.observable(venueData.name);
+            this.venueDesptn = ko.observable(venueData.description);
+            this.venueAddress = ko.observable(venueData.address);
+        }*/
     }
-
-	function VenueViewModel() {  
-    //data
-    var self = this;
-    // Non-editable catalog data - would come from the server
-    self.venues = ko.observableArray([    
-        { venueName: 'Nandos' , venueDesptn: 'Flame-grilled PERi-PERi chicken ', venueAddress: '  811 Hay St'},
-        { venueName: 'Subway' , venueDesptn: 'Eat Fresh', venueAddress: '16a/1490 Albany Hwy'},
-        { venueName: 'Nao Japanese Restaurant' , venueDesptn: 'soup base, ramen noodles', venueAddress: '117 Murray St'}
-
-    ]); 
-   self.newVenue = ko.observable();
-   self.newDesptn = ko.observable();
-   self.newAddress = ko.observable();
-
-   //operations
-   self.addVenue = function() {
-        self.venues.push(new venue({ venueName: this.newVenue(), venueDesptn: this.newDesptn(), venueAddress: this.newAddress()}));
-        self.newVenue("");
-        self.newDesptn("");
-        self.newAddress("");
-     };
-    }
-   
-    ko.applyBindings(new VenueViewModel());
-	
+    ko.applyBindings(new AppViewModel());
 };
 
 $(onReady);
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,10 +1,26 @@
 class VenuesController < ApplicationController
 def index
 	@venues=Venue.all
-	#render :json =>@venues
 	respond_to do |format|
 		format.html {}
 		format.json { render :json =>@venues}
 	end
 end
+
+def new
+	@venue = Venue.new
+end
+
+def create
+	@venue = Venue.new(params[:venue])
+	if @venue.save
+		format.html {redirect_to @venue, notice: 'Venue has been created'}
+		format.js {}
+		format.json {render json: @venue, status: created, location: @venue}
+	else
+		format.html {render :venue}
+		format.json {render json: @venue.errors, status: :unprocessable_entity}
+	end
+end
+
 end
