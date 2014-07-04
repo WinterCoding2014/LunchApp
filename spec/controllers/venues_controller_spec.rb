70070926b50234a043e16f0ea4001fcf42da88cb
Pyrby <pyrbyg@gmail.com>
2014-07-04 15:48:26 +0800

    Added reverse test for A-Z ordering of venue list

--- spec/controllers/venues_controller_spec.rb
+++ spec/controllers/venues_controller_spec.rb
@@ -14,5 +14,14 @@ describe VenuesController do
       expect(response.body).to eq(expected)
     end
 
+    it "should not return venues list out of alphabetical order" do
+      venueB = Venue.create!({name: "B Name"})
+      venueA = Venue.create!({name: "A Name"})
+
+      notExpected = [venueB, venueA].to_json
+      get :index, :format => :json
+      expect(response.body).to_not eq(notExpected)
+    end
+
   end
 end
