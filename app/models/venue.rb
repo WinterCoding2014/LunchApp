cc751d31bfaa4d5b797dfca314c1ec09616c3c84
Pyrby <pyrbyg@gmail.com>
2014-07-08 11:22:54 +0800

    Error messages now have custom style and model error messages modified slightly

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,7 @@
 class Venue < ActiveRecord::Base
-  validates_presence_of :name, :message => "Name cannot be blank"
-  validates_presence_of :description, :message => "Description cannot be blank"
-  validates_presence_of :address, :message => "Address cannot be blank"
+  validates_presence_of :name, :message => "*name cannot be blank"
+  validates_presence_of :description, :message => "*description cannot be blank"
+  validates_presence_of :address, :message => "*address cannot be blank"
 
   scope :sorted, -> { order("upper(name)") }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703062237) do
+ActiveRecord::Schema.define(version: 20140708025205) do
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "venues", force: true do |t|
     t.string   "name"
