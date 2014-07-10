79fb14408aba1321f5e5e1022217ef249f1973bf
Pyrby <pyrbyg@gmail.com>
2014-07-10 12:08:33 +0800

    Rating model now validates score between 0-5

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,5 +2,7 @@ class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :venue
 
-  validates :score, :greater_than_or_equal_to %0, :less_than_or_equal_to %5
+  validates_presence_of :user_id, :message => "*must have a user id"
+  validates_presence_of :venue_id, :message => "*must have a venue id"
+  validates_numericality_of :score, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, :message => 'Score must be between 0-5'
 end
