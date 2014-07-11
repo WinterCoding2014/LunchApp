dfad7c9665f457250a7844b517f46d1e362438ee
Pyrby <pyrbyg@gmail.com>
2014-07-11 08:35:10 +0800

    Fixed issue with database-rating not saving an updated rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,7 +3,11 @@ class RatingsController < ApplicationController
     puts "got to set"
     @venue_id = params[:venue_id]
     @rating = params[:rating]
-    Rating.where(:user_id => current_user.id, :venue_id => @venue_id).first_or_create!(:score => @rating)
+    puts "New rating is " + @rating
+    ratingObj = Rating.where(:user_id => current_user, :venue_id => @venue_id).first_or_create!(:score => @rating)
+    ratingObj.update_attribute(:score, @rating)
+    puts "Score for that rating is "
+    puts Rating.find_by(:user_id => current_user, :venue_id => @venue_id).score
 
     render json: {ok:'yes'}
     # respond_to do |format|
