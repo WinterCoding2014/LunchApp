923c6ec65d12e1ba59be8b4955247f9ecc9a9eca
Pyrby <pyrbyg@gmail.com>
2014-07-11 09:29:12 +0800

    Adjusted sessions controller spec to handle new way of checking user IDs

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,16 +1,9 @@
 class RatingsController < ApplicationController
   def set
-    puts "got to set"
     @venue_id = params[:venue_id]
     @rating = params[:rating]
-    puts "New rating is " + @rating
     ratingObj = Rating.where(:user_id => current_user, :venue_id => @venue_id).first_or_create!(:score => @rating)
     ratingObj.update_attribute(:score, @rating)
-    puts "Score for that rating is "
-    puts Rating.find_by(:user_id => current_user, :venue_id => @venue_id).score
-
     render json: {ok:'yes'}
-    # respond_to do |format|
-    #   format.json { render :jason => @rating }
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,8 +16,8 @@ describe SessionsController do
 
         post :create, shortname: 'roy'
 
-        expect(session[:user]).to_not be_nil
-        expect(session[:user]).to eq(roy)
+        expect(session[:user_id]).to_not be_nil
+        expect(session[:user_id]).to eq(roy.id)
       end
 
     end
@@ -25,14 +25,17 @@ describe SessionsController do
 
   context 'when logged in' do
     before do
-      session[:user] = User.new(email: 'logged-in@thoughtworks.com')
+      user = User.new(email: 'logged-in@thoughtworks.com')
+      user.save
+      session[:user_id] = user.id
+      #User.new(email: 'logged-in@thoughtworks.com')
     end
 
     describe 'DELETE' do
       it 'clears the current_user entry' do
-        expect(session[:user]).to_not be_nil
+        expect(session[:user_id]).to_not be_nil
         delete :destroy
-        expect(session[:user]).to be_nil
+        expect(session[:user_id]).to be_nil
       end
     end
 
