b01557401158b68856410ed6246469edc4723712
Pyrby <pyrbyg@gmail.com>
2014-07-11 09:54:19 +0800

    Venues controller spec now expects ratings [to be zero] and correctly sets the user ID in the session

--- spec/controllers/venues_controller_spec.rb
+++ spec/controllers/venues_controller_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe VenuesController do
 
   before do
-    request.session[:user] = User.new(email: 'logged-in@thoughtworks.com')
+    user = User.new(email: 'logged-in@thoughtworks.com')
+    user.save
+    session[:user_id] = user.id
   end
 
   describe "GET #index" do
@@ -13,9 +15,13 @@ describe VenuesController do
       venueA = Venue.create!({name: "A Name", description: "A Description", address: "A Address"})
       venueC = Venue.create!({name: "C Name", description: "C Description", address: "C Address"})
 
-      expected = ActiveSupport::JSON.decode([venueA, venueB, venueC].to_json)
+      venue_a_properties = {id:venueA.id,name:venueA.name,address:venueA.address,description:venueA.description,rating:0}
+      venue_b_properties = {id:venueB.id,name:venueB.name,address:venueB.address,description:venueB.description,rating:0}
+      venue_c_properties = {id:venueC.id,name:venueC.name,address:venueC.address,description:venueC.description,rating:0}
+
+      expected = ActiveSupport::JSON.decode([venue_a_properties, venue_b_properties, venue_c_properties].to_json)
       get :index, :format => :json
-      actual = ActiveSupport::JSON.decode(response.body).each { |d| d.delete('rating') }
+      actual = ActiveSupport::JSON.decode(response.body)
 
       expect(actual).to eq(expected)
     end
