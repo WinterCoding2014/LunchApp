eb510253d06b3c59222ffc9d01b1bf526fc52601
Jojo Li <joli@thoughtworks.com>
2014-07-15 14:14:32 +0800

    defines the relationships between new tables

--- app/models/lunch_week.rb
+++ app/models/lunch_week.rb
@@ -1,3 +1,4 @@
 class LunchWeek < ActiveRecord::Base
   has_one :chosen_venue
+  validates_presence_of :friday_date
 end
--- app/models/user_utility_log.rb
+++ app/models/user_utility_log.rb
@@ -1,4 +1,5 @@
 class UserUtilityLog < ActiveRecord::Base
   belongs_to :user
   belongs_to :lunch_week
+  validates_presence_of :difference
 end
