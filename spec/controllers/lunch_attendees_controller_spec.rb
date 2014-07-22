55550074013e2a877f1aa703ee2e7d306721fba8
Jojo Li <joli@thoughtworks.com>
2014-07-22 15:01:12 +0800

    write test case for getting for in/notIn status

--- spec/controllers/lunch_attendees_controller_spec.rb
+++ spec/controllers/lunch_attendees_controller_spec.rb
@@ -12,7 +12,7 @@ describe LunchAttendeesController do
 
     it "saves in status of current user and returns ok" do
 
-      this_lunch_week = LunchWeek.create!(:friday_date => Time.zone.today)
+      @this_lunch_week = LunchWeek.create!(:friday_date => Time.zone.today)
 
       expected = {ok: 'yes'}.to_json
       put :set, :format => :json, :attend_status => true
@@ -21,16 +21,43 @@ describe LunchAttendeesController do
     end
     it "delete in status of current user if not attending and returns ok" do
 
-      this_lunch_week = LunchWeek.create!(:friday_date => Time.zone.today)
-      existing_lunch_attendee = LunchAttendee.create!(:user_id => session[:user_id], :lunch_week_id => 1)
+      @this_lunch_week = LunchWeek.create!(:friday_date => Time.zone.today)
+      @existing_lunch_attendee = LunchAttendee.create!(:user_id => session[:user_id], :lunch_week_id => 1)
 
       expected = {ok: 'yes'}.to_json
       put :set, :format => :json, :attend_status => false
       actual = response.body
       expect(actual).to eq(expected)
     end
-
-
   end
 
+
+
+  describe "GET #get" do
+
+    it "returns true if user is in" do
+
+      @this_lunch_week = LunchWeek.create!(:friday_date => Time.zone.today)
+      @existing_lunch_attendee = LunchAttendee.create!(:user_id => session[:user_id], :lunch_week_id => 1)
+      if !@existing_lunch_attendee.nil?
+        @current_status = true
+      else
+        @current_status = false
+      end
+
+      expected = @current_status.to_json
+      get :get, :format => :json
+      actual = response.body
+      expect(actual).to eq(expected)
+    end
+    it "return false if user not in" do
+      @this_lunch_week = LunchWeek.create!(:friday_date => Time.zone.today)
+      @current_status = false
+
+      expected = @current_status.to_json
+      get :get, :format => :json
+      actual = response.body
+      expect(actual).to eq(expected)
+    end
+  end
 end
